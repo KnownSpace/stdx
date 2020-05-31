db74e035a3ec4ec2960865e42d3560ad5b81ff81
孜然 <2145973003@qq.com>
2020-05-31 11:25:14 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -92,6 +92,7 @@ bool handle_request_hello(stdx::http_connection conn, stdx::http_request req)
 	bool keep = req.request_header().is_keepalive();
 	stdx::http_response response(200);
 	add_keepalive(response, keep);
+	response.response_body().push(U("Hello World"));
 	auto t = conn.write(response)
 		.then([conn, keep](stdx::task_result<size_t> r)mutable
 			{
@@ -137,7 +138,7 @@ int main(int argc, char** argv)
 			stdx::cancel_token token;
 			conn.read_until(token, [token,conn,file_io_service](stdx::http_request req) mutable
 			{
-				if (!handle_request(conn, req,file_io_service))
+				if (!handle_request_hello(conn, req))
 				{
 					token.cancel();
 				}
